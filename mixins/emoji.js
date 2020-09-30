7ca144f398e410ba5c5fc28cba4c3e97b13ffc49
wendy.li <wendy.li@wanin.tw>
2020-09-30 23:44:27 +0800

    新增表情符號

--- mixins/emoji.js
+++ mixins/emoji.js
@@ -18,5 +18,7 @@ export const emojiData = [
     { data: "_img/_emoji/category01/e00000.png", text: "e0000", category: "category01" },
     { data: "_img/_emoji/category01/e00001.png", text: "e0001", category: "category01" },
     { data: "_img/_emoji/category01/e00002.png", text: "e0002", category: "category01" },
+    { data: "_img/_emoji/category01/e00003.png", text: "e0003", category: "category01" },
+    { data: "_img/_emoji/category01/e00004.png", text: "e0004", category: "category01" },
     { data: "_img/_emoji/sticker01/s0001.png", text: "s0001", category: "stickers01" }
 ];
